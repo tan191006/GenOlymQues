56835c97db0c737cf2c19092105cf7267a418c78
tranvankhuyen <khuyentvhe187268@fpt.edu.vn>
2024-01-04 00:44:14 +0700

    feat: export data

--- src/app/page.js
+++ src/app/page.js
@@ -5,59 +5,69 @@ import { TopicChose } from '@/components/sections/TopicChose';
 import { TopicChoseForPart2 } from '@/components/sections/TopicChoseForPart2';
 import { Result } from '@/components/sections/Result';
 import { Button } from '@/components/Button';
+const XLSX = require("xlsx");
 
 export default function Home() {
     const data = {
         part1: [
             {
+                id: 1,
                 question:
                     'What is the role of a firewall in computer security?',
                 answer: 'Lorem lorem',
                 topic: 'HTML',
             },
             {
+                id: 2,
                 question:
                     'What is the role of a firewall in computer security?',
                 answer: 'Lorem lorem',
                 topic: 'HTML',
             },
             {
+                id: 3,
                 question:
                     'What is the role of a firewall in computer security?',
                 answer: 'Lorem lorem',
                 topic: 'HTML',
             },
             {
+                id: 4,
                 question:
                     'What is the role of a firewall in computer security?',
                 answer: 'Lorem lorem',
                 topic: 'HTML',
             },
             {
+                id: 5,
                 question:
                     'What is the role of a firewall in computer security?',
                 answer: 'Lorem lorem',
                 topic: 'HTML',
             },
             {
+                id: 6,
                 question:
                     'What is the role of a firewall in computer security?',
                 answer: 'Lorem lorem',
                 topic: 'HTML',
             },
             {
+                id: 7,
                 question:
                     'What is the role of a firewall in computer security?',
                 answer: 'Lorem lorem',
                 topic: 'HTML',
             },
             {
+                id: 8,
                 question:
                     'What is the role of a firewall in computer security?',
                 answer: 'Lorem lorem',
                 topic: 'HTML',
             },
             {
+                id: 9,
                 question:
                     'What is the role of a firewall in computer security?',
                 answer: 'Lorem lorem',
@@ -148,6 +158,22 @@ export default function Home() {
         ],
     };
 
+
+    //export data to excel
+    const generateWorksheet = (data) => {
+        const ws = XLSX.utils.json_to_sheet(data)
+        return ws
+    }
+
+    const exportData = () => {
+        let wb = XLSX.utils.book_new()
+        for(const part in data) {
+            const ws = generateWorksheet(data[part])
+            XLSX.utils.book_append_sheet(wb, ws, part)
+        }
+        XLSX.writeFile(wb, "Data.xlsx")
+    }
+
     // input tags
     const [tags, setTags] = useState([]);
     // input tags set Done
@@ -172,23 +198,27 @@ export default function Home() {
             {data.part2 && <Result part="Part 2" data={data.part2} />}
             {data.part3 && <Result part="Part 3" data={data.part3} />}
             {data.part4 && <Result part="Part 4" data={data.part4} />}
-            <div className="fixed bottom-0 right-0 flex items-center px-10 py-5">
-                <Button
-                    onClick={() => {
-                        window.scrollBy(0, window.innerHeight - 100);
-                    }}
-                >
-                    <FaRegArrowAltCircleDown />
-                </Button>
+            <div className="fixed bottom-0 right-0 left-0 flex justify-between items-center px-10 py-5">
+                <div className='flex'>
+                    <Button
+                        onClick={() => {
+                            window.scrollBy(0, window.innerHeight - 100);
+                        }}
+                    >
+                        <FaRegArrowAltCircleDown />
+                    </Button>
 
-                <Button
-                    onClick={() => {
-                        window.scrollBy(0, -window.innerHeight + 100);
-                    }}
-                >
-                    <FaRegArrowAltCircleUp />
-                </Button>
+                    <Button
+                        onClick={() => {
+                            window.scrollBy(0, -window.innerHeight + 100);
+                        }}
+                    >
+                        <FaRegArrowAltCircleUp />
+                    </Button>
+                </div>
+                <Button className={"ml-5"} onClick={exportData}>Download</Button>
             </div>
+
         </div>
     );
 }
